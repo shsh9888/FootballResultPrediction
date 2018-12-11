fd7e6b0805a9a12fd7e6f470f159c74a65fec125
Keval D Shah <keval.shah@colorado.edu>
2018-12-11 01:25:09 -0700

    Added player and team overall to the FV

--- readCSV.py
+++ readCSV.py
@@ -5,6 +5,82 @@
 
 import csv
 
+# Team mappings
+teamNames = {
+    'Blackpool' : 'Blackpool',
+    'Wigan Athletic' : 'Wigan',
+    'Bolton Wanderers' : 'Bolton',
+    'Fulham' : 'Fulham',
+    'Aston Villa' : 'Aston Villa',
+    'West Ham United' : 'West Ham',
+    'Blackburn Rovers' : 'Blackburn',
+    'Everton' : 'Everton',
+    'Watford' : 'Watford',
+    'West Bromwich Albion' : 'West Brom',
+    'Tottenham Hotspur' : 'Tottenham',
+    'Queens Park Rangers' : 'QPR',
+    'Arsenal' : 'Arsenal',
+    'Norwich City' : 'Norwich',
+    'Chelsea' : 'Chelsea',
+    'Bournemouth' : 'Bournemouth',
+    'Manchester United' : 'Man United',
+    'Leicester City' : 'Leicester',
+    'Newcastle United' : 'Newcastle',
+    'Crystal Palace' : 'Crystal Palace',
+    'Southampton' : 'Southampton',
+    'Manchester City' : 'Man City',
+    'Stoke City' : 'Stoke',
+    'Sunderland' : 'Sunderland',
+    'Swansea City' : 'Swansea',
+    'Cardiff City' : 'Cardiff',
+    'Burnley' : 'Burnley',
+    'Hull City' : 'Hull',
+    'Portsmouth' : 'Portsmouth',
+    'Middlesbrough' : 'Middlesbrough',
+    'Liverpool' : 'Liverpool',
+    'Reading' : 'Reading',
+    'Wolverhampton Wanderers' : 'Wolves',
+    'Birmingham City' : 'Birmingham'
+}
+
+# Team mappings
+teamNamesRev = {
+    'Blackpool' : 'Blackpool',
+    'Wigan' : 'Wigan Athletic',
+    'Bolton' : 'Bolton Wanderers',
+    'Fulham' : 'Fulham',
+    'Aston Villa' : 'Aston Villa',
+    'West Ham' : 'West Ham United',
+    'Blackburn' : 'Blackburn Rovers',
+    'Everton' : 'Everton',
+    'Watford' : 'Watford',
+    'West Brom' : 'West Bromwich Albion',
+    'Tottenham' : 'Tottenham Hotspur',
+    'QPR' : 'Queens Park Rangers',
+    'Arsenal' : 'Arsenal',
+    'Norwich' : 'Norwich City',
+    'Chelsea' : 'Chelsea',
+    'Bournemouth' : 'Bournemouth',
+    'Man United' : 'Manchester United',
+    'Leicester' : 'Leicester City',
+    'Newcastle' : 'Newcastle United',
+    'Crystal Palace' : 'Crystal Palace',
+    'Southampton' : 'Southampton',
+    'Man City' : 'Manchester City',
+    'Stoke' : 'Stoke City',
+    'Sunderland' : 'Sunderland',
+    'Swansea' : 'Swansea City',
+    'Cardiff' : 'Cardiff City',
+    'Burnley' : 'Burnley',
+    'Hull' : 'Hull City',
+    'Portsmouth' : 'Portsmouth',
+    'Middlesbrough' : 'Middlesbrough',
+    'Liverpool' : 'Liverpool',
+    'Reading' : 'Reading',
+    'Wolves' : 'Wolverhampton Wanderers',
+    'Birmingham' : 'Birmingham City'
+}
+
 playerD = {}
 teamD = {}
 matchD = {}
@@ -56,7 +132,7 @@ csv_file.close()
 # print a match instance
 # print (matchD[matchD.keys()[0]])
 
-csv_file = open('Data/finalData.csv')
+csv_file = open('Data/final_processed_1.csv')
 csv_reader = csv.reader(csv_file, delimiter=',')
 line_count = 0
 fv_cols = []
@@ -66,7 +142,7 @@ for row in csv_reader:
         print (fv_cols)
         line_count += 1
     else:
-        fv[row[1]+row[2]+row[3]] = row
+        fv[row[1]] = row
         line_count += 1
 csv_file.close()
 
@@ -140,68 +216,62 @@ for mID, aMatch in matchD.items():
 printDataDetail(matchD, match_cols, [10])
 
 
-# CODE FOR EXTRACTING UNIQUE TEAMS
-uniq = []
-for key, val in matchD.items():
-    if val[2] == '1729' and val not in uniq:
-        uniq.append(val)
+''' CODE FOR EXTRACTING UNIQUE TEAMS '''
+# uniq = []
+# for key, val in matchD.items():
+#     if val[2] == '1729' and val not in uniq:
+#         uniq.append(val)
 
-uTeams = []
-for uid in uniq:
-    if uid[7] not in uTeams:
-        uTeams.append(uid[7])
-    if uid[8] not in uTeams:
-        uTeams.append(uid[8])
-print ((uTeams))
-
-# Team mappings
-teamNames = {
-    'Blackpool' : 'Blackpool',
-    'Wigan Athletic' : 'Wigan',
-    'Bolton Wanderers' : 'Bolton',
-    'Fulham' : 'Fulham',
-    'Aston Villa' : 'Aston Villa',
-    'West Ham United' : 'West Ham',
-    'Blackburn Rovers' : 'Blackburn',
-    'Everton' : 'Everton',
-    'Watford' : 'Watford',
-    'West Bromwich Albion' : 'West Brom',
-    'Tottenham Hotspur' : 'Tottenham',
-    'Queens Park Rangers' : 'QPR',
-    'Arsenal' : 'Arsenal',
-    'Norwich City' : 'Norwich',
-    'Chelsea' : 'Chelsea',
-    'Bournemouth' : 'Bournemouth',
-    'Manchester United' : 'Man United',
-    'Leicester City' : 'Leicester',
-    'Newcastle United' : 'Newcastle',
-    'Crystal Palace' : 'Crystal Palace',
-    'Southampton' : 'Southampton',
-    'Manchester City' : 'Man City',
-    'Stoke City' : 'Stoke',
-    'Sunderland' : 'Sunderland',
-    'Swansea City' : 'Swansea',
-    'Cardiff City' : 'Cardiff',
-    'Burnley' : 'Burnley',
-    'Hull City' : 'Hull',
-    'Portsmouth' : 'Portsmouth',
-    'Middlesbrough' : 'Middlesbrough',
-    'Liverpool' : 'Liverpool',
-    'Reading' : 'Reading',
-    'Wolverhampton Wanderers' : 'Wolves',
-    'Birmingham City' : 'Birmingham'
-}
+# uTeams = []
+# for uid in uniq:
+#     if uid[7] not in uTeams:
+#         uTeams.append(uid[7])
+#     if uid[8] not in uTeams:
+#         uTeams.append(uid[8])
+# print ((uTeams))
 
 '''
 The next and final step is to integrate the attributes from matches to the fv
+For this I will create a new dictionary for matches with the key as (home_away_date)
 '''
 
-#Print an item from the feature vector
+matchesWithKey = {}
 
+for key, val in matchD.items():
+    matchHomeT = teamNames[val[7]]
+    matchAwayT = teamNames[val[8]]
+    matchDate = val[5][:10]
+    key = matchHomeT+'_'+matchAwayT+'_'+matchDate
+    matchesWithKey[key] = val
+
+# print (matchesWithKey.keys()[0])
+
+#Print an item from the feature vector
 # oneItem  = fv[fv.keys()[0]]
 # thisDate = oneItem[1]
-
 # print (oneItem)
 
-# for key, val in fv.items():
-#     print (key, val)
+# Add the column names to the header
+for i in range(1, 12):
+    fv_cols.append("home_player_"+str(i))
+for i in range(1, 12):
+    fv_cols.append("away_player_"+str(i))
+fv_cols.append("home_team_overall")
+fv_cols.append("away_team_overall")
+# print (fv_cols)
+
+# Finally append the attributes!
+for key, val in fv.items():
+    add22Players = matchesWithKey[key]
+    homeAttr = teamOverall_name[teamNamesRev[val[2]]]
+    awayAttr = teamOverall_name[teamNamesRev[val[3]]]
+    fv[key] += add22Players[55:77] + [str(homeAttr)] + [str(awayAttr)]
+
+import csv
+
+enhancedDataFile = open('final_processed_2.csv', mode='w')
+enhancedDataFile = csv.writer(enhancedDataFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+enhancedDataFile.writerow(fv_cols)
+for val in fv.values():
+    enhancedDataFile.writerow(val)
